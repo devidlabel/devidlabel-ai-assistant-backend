6c271a7bf3f4fbb6e9bb2ee7067419406047a670
devidlabel <devidlabelshop@gmail.com>
2026-07-28 18:55:57 +0200

    Route Klaviyo reporting through worker entrypoint

---
+++ src/worker-v3.ts
@@ -0,0 +1,17 @@
+import assistantWorker from "./worker-v2";
+import { handleKlaviyoReportingRequest } from "./klaviyo-reporting";
+
+type WorkerEnv = Parameters<typeof assistantWorker.fetch>[1] & {
+  KLAVIYO_PRIVATE_API_KEY?: string;
+  KLAVIYO_REPORT_ACCESS_TOKEN?: string;
+  KLAVIYO_CONVERSION_METRIC_ID?: string;
+};
+type WorkerExecutionContext = Parameters<typeof assistantWorker.fetch>[2];
+
+export default {
+  async fetch(request: Request, env: WorkerEnv, context: WorkerExecutionContext): Promise<Response> {
+    const klaviyoResponse = await handleKlaviyoReportingRequest(request, env);
+    if (klaviyoResponse) return klaviyoResponse;
+    return assistantWorker.fetch(request, env, context);
+  },
+};
